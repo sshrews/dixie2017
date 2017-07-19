8a4052f46b12c9e2bcab1ff1a9cb1494195495e8
Scott Shrewsbury <scottshrewsbury@gmail.com>
2017-07-18 22:55:47 -0400

    getting aws working with artist pics

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,3 @@
+$(document).on("click", ".bg-info", function(e){
+   $(this).hide();
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623163741) do
+ActiveRecord::Schema.define(version: 20170624223528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20170623163741) do
     t.string "name"
     t.string "artist1"
     t.string "artist2"
-    t.string "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "cover"
   end
 
 end
